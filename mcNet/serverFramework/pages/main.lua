0be53f95bd205359a268b5de02a7fba4eac1e92b
Redtech0inc <florian.maximilian.leopold@gmail.com>
2025-06-29 23:12:20 +0200

    Update main.lua

--- mcNet/serverFramework/pages/main.lua
+++ mcNet/serverFramework/pages/main.lua
@@ -1,5 +1,5 @@
---this "libs" comment to gether with the "/libs" comment defines what libraries
---"import" tells the program what libraries exactly
+--this "libs" comment to gether with the "/libs" comment defines the start of library definition
+--"import" tells the program what libraries to import exactly
 --libs
 --import<libs/testLib.lua>
 --/libs
@@ -9,23 +9,36 @@
 
 --[[
 return codes:
-+------+----------------+--------------------------+
-| code | action         | additional requirements  |
-+------+----------------+--------------------------+
-| -1   | ends page      | None                     |
-|  1   | loads new page | must return page message | --the message to give the server to download the page will be  shown in the server.lua file
-|      |                |                          |
-+-----------------------+--------------------------+
++------+----------------------+--------------------------+
+| code | action               | additional requirements  |
++------+----------------------+--------------------------+
+| -3   | Ends net program     | None                     |
+| -2   | Ends page            | None                     |
+| -1   | Goes back 1 page     | None                     |
+|  1   | Reloads current page | None                     |
+|  2   | Loads new page       | Must return page message | --the message to give the server to download the page will be  shown in the server.lua file
++-----------------------------+--------------------------+
 ]]
 
 local something,somethingSprite
 local sizeX,sizeY = term.getSize()
 
+--disconnect runs once the client returns to the home page or exits the program (won't run when terminated)
+disconnect = function()
+    systemOut:print("hello world2")
+end
+
+--hudUsed runs when the hud registered a mouse_click event
+hubUsed = function()
+    systemOut:print("hello world")
+end
+
+--init runs once upon startup
 init = function (path)
     os.loadAPI(path.."libs/testLib.lua")--on receiving computer will find root and then go back one folder to enter apis folder where testLib.lua was downloaded to
 
     somethingSprite = openUILib.getShapeSprite(colors.blue,"triangle",5,5)
-    something = openUILib.sprite:addSprite(somethingSprite,nil,2,2)
+    something = openUILib.sprite:addSprite(somethingSprite,nil,10,2)
 
     local background = {}
     for i=0,sizeX do
@@ -38,12 +51,13 @@ init = function (path)
     openUILib.setBackgroundImage(background)
 end
 
+--main repeatedly runs in a loop together with the hud (if hud is used will stop)
 main = function(path)--there are currently 1 argument. this argument is the root path of the mcNet client
-    for _=1,20 do
-        testLib.test()
+    for i=1,math.random(30) do
+        testLib.test(i)
         somethingSprite = openUILib.turnSprite(somethingSprite,1)
         something:changeSpriteData(somethingSprite)
         openUILib.render()
-        sleep(0.5)
+        sleep(0.1)
     end
 end
